483d866b05ef542950d5c9195a811172bbb62d91
Alexis Kafantaris-Leukas <57844211+fadewell@users.noreply.github.com>
2022-11-05 23:05:33 +0200

    Baseline for line segmentation

--- AutA.py
+++ AutA.py
@@ -7,12 +7,10 @@ Created on Sat Nov  5 16:12:22 2022
 """
 
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
 import cv2
 
 import os
+
 
 #https://www.folkstalk.com/tech/draw-bounding-box-on-image-python-opencv-with-code-examples/
 
@@ -22,34 +20,37 @@ me = 1
 
 img  = cv2.imread(jpeg)
 img = cv2.resize(img, None, fx=fix  , fy=me , interpolation=5 )
+img =cv2.resize(img, (740,550))
+
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 15)
+img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 81, 31)
 
 contours, _ = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
 
-cv2.imshow("Show",img)
-cv2.waitKey()  
-    
-
 def canny(image):
-    return cv2.Canny(image, 500, 1200) 
+    return cv2.Canny(image, 500 , 1500 ) 
     
 # https://stackoverflow.com/questions/47627182/detecting-interword-space-in-ocr-using-python-and-opencv 
 # idea for spaces
 img = canny(img)
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (31, 2))
 img = cv2.dilate(img, kernel)
+
+cv2.imshow("Show",img)
+cv2.waitKey()  
+    
 #https://stackoverflow.com/questions/43053923/replace-black-by-white-and-white-by-black-in-images
 img = cv2.subtract(255, img) 
 
 contours, _ = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
 img2  = cv2.imread(jpeg)
-img2 = cv2.resize(img2, None, fx=fix  , fy=me , interpolation=5)
+img2 = cv2.resize(img2, None, fx = fix  , fy= me , interpolation=5)
 img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-img2 = cv2.adaptiveThreshold(img2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31,11  )
+img2 = cv2.adaptiveThreshold(img2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 41,11)
+img2 =cv2.resize(img2, (740,550))
 
 cv2.imshow("Show",img)
 cv2.waitKey()  
@@ -61,7 +62,7 @@ lie = 0
 Y,U = [],[]
 for c in contours:
     rect = cv2.boundingRect(c)
-    if rect[2] < 50 or rect[3] < 50: continue
+    if rect[2] < 20 or rect[3] < 20: continue
    # print (cv2.contourArea(c))
     x,y,w,h = rect
     cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
